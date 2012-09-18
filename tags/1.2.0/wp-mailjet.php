cfe366eed19b6a160338f9fbd52c027a2b16fe2d
Mailjet <Mailjet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-18 08:32:32 +0000

    v++
    git-svn-id: https://plugins.svn.wordpress.org/mailjet-for-wordpress@600382 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.2.0/wp-mailjet.php
+++ tags/1.2.0/wp-mailjet.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Mailjet for Wordpress
-Version: 1.1.6
+Version: 1.2.0
 Plugin URI: https://www.mailjet.com/plugin/wordpress.htm
 Description: Use mailjet SMTP to send email, manage lists and contacts within wordpress
 Author: Mailjet SAS
