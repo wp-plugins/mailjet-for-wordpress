39f33e81de676e3fcc4354db385d848a120e9cf4
Mailjet <Mailjet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 13:39:10 +0000

    updated readme
    git-svn-id: https://plugins.svn.wordpress.org/mailjet-for-wordpress@949641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.3.0/views/add_contact.php
@@ -0,0 +1,7 @@
+<form method="post" action="options.php">
+	<div id="titlewrap">
+		<label class="hide-if-no-js" style="" id="title-prompt-text" for="title">Enter title here</label>
+		<input type="text" name="post_title" size="30" tabindex="1" value="" id="title" autocomplete="off">
+	</div>
+<?php submit_button(); ?>
+</form>
